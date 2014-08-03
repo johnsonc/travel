0b6f31bb98a5fbb361dcdf885c698b61fb175e8f
david <dakrauth@gmail.com>
2014-08-03 19:30:00 +0200

    profile updates

--- travel/media/js/travel.js
+++ travel/media/js/travel.js
@@ -1,6 +1,33 @@
+//------------------------------------------------------------------------------
+// Sample entity object
+//------------------------------------------------------------------------------
+// entity = {
+//    "rating": 3,
+//    "entity_id": 1463,
+//    "code": "BUD",
+//    "type_title": "Airport",
+//    "num_visits": 1,
+//    "flag_url": "img/ap-32.png",
+//    "type_abbr": "ap",
+//    "id": 276
+//    "country_code": "HU",
+//    "country_name": "Hungary",
+//    "most_recent_visit": "2013-12-29 08:00:00",
+//    "first_visit": "2013-12-29 08:00:00",
+//    "name": "Budapest Ferenc Liszt International Airport"
+// }
+//------------------------------------------------------------------------------
+
 ;var Travelogue = (function($) {
     var root = this;
-    var STARS = '★★★★★';
+    var $$ = _.bind(document.getElementById, document);
+    
+    var profile_datepicker_opts = {
+        changeMonth: true,
+        changeYear: true,
+        showButtonPanel: true,
+        yearRange: 'c-50:nnnn'
+    };
     
     var sorters = {
         'type': function(a, b) {
@@ -27,23 +54,22 @@
         'rating': function(a, b) { return a.rating - b.rating; }
     };
     
-    var profile_history = root.profile_history = {
+    //--------------------------------------------------------------------------
+    var stars = (function() {
+        var STARS = '★★★★★';
+        return function(rating) { return STARS.substr(rating - 1); };
+    }());
+    
+    //--------------------------------------------------------------------------
+    var profile_history = {
         selector: '#history tbody',
         media_prefix: '/media/',
         initialize: function(history, conf) {
             var media_prefix = conf.media_prefix || this.media_prefix;
-            var $co_opts = $('#id_country');
+            var $co_opts = $('#id_co');
             this.filters = {'type': null, 'country_code': null};
             this.countries = {};
             this.all_entities = _.map(history, function(e) {
-                // "rating": 3,             "entity_id": 1463,
-                // "code": "BUD",           "type_title": "Airport",
-                // "num_visits": 1,         "flag_url": "img/ap-32.png",
-                // "type_abbr": "ap",       "id": 276
-                // "country_code": "HU",    "country_name": "Hungary",
-                // "most_recent_visit": "2013-12-29 08:00:00",
-                // "first_visit": "2013-12-29 08:00:00",
-                // "name": "Budapest Ferenc Liszt International Airport",
                 e.entity_url = entity_url(e);
                 e.flag_url = e.flag_url ? media_prefix + e.flag_url: '';
                 e.most_recent_visit = new Date(e.most_recent_visit);
@@ -55,26 +81,53 @@
                 return e;
             }, this);
             
-            _.each(this.countries, function(value, key) {
-                $co_opts.append('<option value="co-' + key + '">' + value + '</option>');
-            });
+            
+            _.each(
+                _.pairs(this.countries).sort(function(a,b) {
+                    return (a[1] > b[1]) ? 1 : (a[1] < b[1] ? -1 : 0); 
+                }),
+                function(item) {
+                    $co_opts.append('<option value="' + item[0] + '">' + item[1] + '</option>');
+                }
+            );
             
             this.$el = $('#history');
             this.$el.find('thead th').click(sort_handler);
         },
 
-        filter: function(type, country_code) {
+        filter: function(bits) {
+            var type     = bits.type;
+            var co       = bits.co;
+            var dt       = bits.date ? new Date(bits.date) : null;
+            var tf       = bits.timeframe;
             var entities = this.all_entities;
-            this.filters = {'type': type, 'country_code': country_code};
-            if(type || country_code) {
+            
+            console.log('filter bits', bits);
+            this.filters = {'type': type, 'co': co};
+            if(type || co || dt) {
                 entities = _.filter(entities, function(e) {
                     var good = true;
                     if(type) {
                         good &= (e.type_abbr === type);
                     }
 
-                    if(country_code) {
-                        good &= (e.country_code === country_code);
+                    if(co) {
+                        good &= (
+                            e.country_code === co ||
+                            (e.type_abbr === 'co' && e.code === co)
+                        );
+                    }
+                    
+                    if(tf && dt) {
+                        switch(tf) {
+                            case '+':
+                                good &= (e.most_recent_visit >= dt);
+                            break
+                            
+                            case '-':
+                                good &= (e.most_recent_visit <= dt);
+                            break
+                        }
                     }
 
                     return !!good;
@@ -103,63 +156,117 @@
     
 
     //--------------------------------------------------------------------------
-    var change_hash = function(bits) {
-        var hash = bits.length ? '#' + bits.join('+') : './';
-        // console.log('hash', hash);
+    var make_hash = function(bits) {
+        var a = []
+        bits.type && a.push('type', bits.type);
+        bits.co   && a.push('co', bits.co);
+        if(bits.date && bits.timeframe) {
+            a.push('date', bits.timeframe + bits.date);
+        }
+
+        return a.length ? '#' + a.join('/') : './';
+    };
+    
+    //--------------------------------------------------------------------------
+    var obj_from_hash = function(hash) {
+        var obj = {};
+        if(hash) {
+            var a = hash.split('/');
+            for(var i = 0, j = a.length; i < j; i += 2) {
+                if(a[i]) {
+                    obj[a[i]] = a[i + 1];
+                }
+            }
+            if(obj.date) {
+                obj.timeframe = obj.date[0];
+                obj.date = obj.date.substr(1);
+            }
+        }
+        return obj;
+    };
+    
+    //--------------------------------------------------------------------------
+    var update_hash = function(bits) {
+        var hash = make_hash(bits);
+        console.log('hash', hash);
         window.history.pushState({}, '', hash);
     };
     
     //--------------------------------------------------------------------------
+    var pad = function(n) {
+        return n < 10 ? '0' + n : n;
+    }
+
+    //--------------------------------------------------------------------------
+    var get_datepicker_iso = function() {
+        var dt = $('#id_date').datepicker('getDate');
+        if(dt) {
+            return dt.getFullYear()
+                 + '-' + pad(dt.getMonth() + 1)
+                 + '-' + pad(dt.getDate());
+        }
+        return '';
+    }
+    
+    //--------------------------------------------------------------------------
+    var get_filter_bits = function() {
+        return {
+            type:      $$('id_type').value,
+            co:        $$('id_co').value,
+            timeframe: $$('id_timeframe').value,
+            date:      get_datepicker_iso()
+        };
+    };
+    
+    //--------------------------------------------------------------------------
     var on_filter_change = function() {
-        var bits = [];
-        var type = document.getElementById('id_filter').value;
-        var country = document.getElementById('id_country').value;
+        var bits = get_filter_bits();
+        console.log(bits);
         
-        if(type) {
-            bits.push(type);
-            type = type.substr(5);
-        }
-        
-        if(country) {
-            bits.push(country);
-            country = country.substr('3')
-        }
-        
-        change_hash(bits);
-        profile_history.filter(type, country);
+        update_hash(bits);
+        profile_history.filter(bits);
     };
     
     //--------------------------------------------------------------------------
     var on_hash_change = function() {
-        var $filters = $('.filter_ctrl');
-        var params = {'type': null, 'co': null};
-        var bits;
-        var hash = window.location.hash
-                 ? window.location.hash.substring(1).split('+')
-                 : [];
-                 
-        for(var i = 0, j = hash.length; i < j; i++) {
-            bits = hash[i].split('-');
-            params[bits[0]] = bits[1];
-        }
+        var hash = window.location.hash;
+        var bits = obj_from_hash(hash.length > 1 ? hash.substr(1) : '');
+        var dt = $$('id_date');
 
-        // console.log('starting hash', hash);
-        $filters.each(function() {
-            this.value = '';
-        });
+        console.log('bits', bits);
+        $('.filter_ctrl').each(function() { this.value = ''; });
         
-        _.each(hash, function(o) {
-            var sel = 'option[value="' + o + '"]';
-            $filters.find(sel).parent().val(o)
-        });
-        profile_history.filter(params.type, params.co);
+        if(bits.type) {
+            $$('id_type').value = bits.type;
+        }
+        if(bits.co) {
+            $$('id_co').value = bits.co;
+        }
+        if(bits.timeframe) {
+            $$('id_timeframe').value = bits.timeframe;
+        }
+        if(bits.date) {
+            dt.value = bits.date;
+            
+        }
+        else {
+            dt.style.display = 'none';
+        }
+        profile_history.filter(bits);
     };
 
     //--------------------------------------------------------------------------
     var init_profile_filter = function() {
         $(window).on('hashchange', on_hash_change);
         $('.filter_ctrl').on('change', on_filter_change);
+        $('#id_timeframe').on('change', function() {
+            $$('id_date').style.display = this.value ? 'inline-block' : 'none';
+        })
         on_hash_change();
+        $('#id_date')
+            .datepicker(profile_datepicker_opts)
+            .on('change input propertychange', on_filter_change);
+        
     };
     
     //--------------------------------------------------------------------------
@@ -184,43 +291,50 @@
         }
         
         return (
-            '<nobr>'            +
-            str[1] + ' '        +
-            dt.getDate() + ', ' +
-            dt.getFullYear()    +
-            '</nobr><br><nobr>' +
-            str[0] + ' '        +
-            hours  + ':'        +
-            minutes             +
-            ampm + '.m.</nobr>'
+            make_tag('nobr', str[1] + ' ', dt.getDate() + ', ', dt.getFullYear(), '<br>')
+          + make_tag('nobr', str[0] + ' ', hours  + ':', minutes, ampm, '.m.')
         );
     };
     
     //--------------------------------------------------------------------------
-    var make_tag = function(name, html) {
-        return '<' + name + '>' + html + '</' + name + '>';
+    var make_tag = function(name) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        var tag = '<' + name;
+        var attrs;
+        if(typeof args[0] === 'object') {
+            attrs = args.shift();
+            for(var key in attrs) {
+                if(attrs.hasOwnProperty(key)) {
+                    tag += ' ' + key + '="' + attrs[key] + '"'
+                }
+            }
+        }
+        return tag + '>' + args.join('') + '</' + name + '>';
     };
     
     //--------------------------------------------------------------------------
     var make_entity_row = function(e) {
-        var html = '<tr class="type-' + e.type_abbr;
+        var html = '';
+        var attrs = {'data-id' : e.id, 'class': e.type_abbr};
         if(e.country_code) {
-            html += ' co-' + e.country_code;
+            attrs['class'] += ' co-' + e.country_code;
+        }
+        else if(e.type_abbr == 'co') {
+            attrs['class'] += ' co-' + e.code;
         }
         
-        html += '" data-id="' + e.id + '">';
         html += make_tag('td', e.flag_url
           ? '<img class="flag" src="' + e.flag_url + '" />'
           : ''
         );
         
         html += make_tag('td', e.type_title);
-        html += make_tag('td', '<a href="' + e.entity_url + '">' + e.name + '</a>');
+        html += make_tag('td', '<a href="', e.entity_url, '">', e.name, '</a>');
         html += make_tag('td', date_html(e.most_recent_visit));
         html += make_tag('td', date_html(e.first_visit));
         html += make_tag('td', e.num_visits);
-        html += make_tag('td', STARS.substr(e.rating - 1));
-        return html + '</tr>\n';
+        html += make_tag('td', stars(e.rating));
+        return make_tag('tr', attrs, html);
     };
     
     //--------------------------------------------------------------------------
@@ -246,10 +360,10 @@
     };
     
     return {
+        profile_history: profile_history,
         initialize: function(history, conf) {
             profile_history.initialize(history, conf);
             init_profile_filter();
-            
         }
     };
 }(jQuery));
